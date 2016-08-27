8057c70cacdd3d7eccde33c6bf90d5f38d79326e
droz36 <droz_36@hotmail.com>
2016-08-27 00:05:49 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -57,7 +57,7 @@ node localhost.localdomain {
     server_string => $::hostname,
     security      => 'share',
   }
-  samba::server::share { heira('samba::server::share'):
+  samba::server::share { hiera('samba::server::share'):
    comment => hiera('samba::server::share::comment'),
    path    => hiera('samba::server::share::path'),
   }
