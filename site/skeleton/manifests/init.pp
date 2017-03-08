6f66a448516fc038cbfd40de754d803811795957
droz36 <droz_36@hotmail.com>
2017-03-08 10:10:57 -0600

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,3 +1,10 @@
 class skeleton {
   notify  {"Hello, my name is ${::hostname}. This totally worked.":}
+  
+  case $::osfamily {
+    'windows': {
+      notify {"Windows OS detected. I Could do windows specific steps here."
+    }
+  }
+  
 }
