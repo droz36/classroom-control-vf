1321f9a16583b7b598944fe672ca5afe0243a932
Jonathan Drozdowski <Jonathan Drozdowski>
2017-03-10 21:48:39 -0600

    removed white space

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,9 +1,13 @@
+# skeleton of a class
 class skeleton {
   notify  {"Hello, my name is ${::hostname}. This totally worked.":}
-  
+
   case $::osfamily {
     'windows': {
-      notify {"Windows OS detected. I Could do windows specific steps here.":}
+      notify {'Windows OS detected. I Could do windows specific steps here.':}
+    }
+    default: {
+      notify {'Could not detect $::osfamily'}
     }
   }
 }
